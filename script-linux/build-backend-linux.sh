33392d80af32416a7816f5560394b37ffb402050
jinsoo-youn <59471453+jinsoo-youn@users.noreply.github.com>
2020-09-23 15:01:45 +0900

    Add files via upload

--- script-linux/build-backend-linux.sh
+++ script-linux/build-backend-linux.sh
@@ -9,6 +9,8 @@ PROJECT_DIR=$(basename ${PWD})
 
 # GIT_TAG=`git describe --always --tags HEAD`
 GIT_TAG="${PRODUCT}:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${HOTFIX_VERSION}"
+echo ${GIT_TAG}
 LD_FLAGS="-w -X github.com/openshift/console/version.Version=${GIT_TAG}"
+echo ${LD_FLAGS}
 
 CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o bin/bridge github.com/openshift/console/cmd/bridge
