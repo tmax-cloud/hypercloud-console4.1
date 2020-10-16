cd2aab931b8ba9c0d1c0bfe64d3ec12165492144
심소영 <soyoung_shim@tmax.co.kr>
2020-10-16 10:56:16 +0900

    [refactor] 워크플로우 템플릿 코드 정렬
    [IMS] N/A

--- frontend/public/components/workflow-template.jsx
+++ frontend/public/components/workflow-template.jsx
@@ -10,78 +10,71 @@ import { ResourcePlural } from './utils/lang/resource-plural';
 const menuActions = [...Cog.factory.common];
 
 const WorkflowTemplateHeader = props => {
-    const { t } = useTranslation();
-    return (
-        <ListHeader>
-            <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
-                {t('CONTENT:NAME')}
-            </ColHead>
-            <ColHead {...props} className="col-xs-2 col-sm-2" sortField="metadata.namespace">
-                {t('CONTENT:NAMESPACE')}
-            </ColHead>
-            <ColHead {...props} className="col-sm-2 hidden-xs" sortField="metadata.creationTimestamp">
-                {t('CONTENT:CREATED')}
-            </ColHead>
-        </ListHeader>
-    );
+  const { t } = useTranslation();
+  return (
+    <ListHeader>
+      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
+        {t('CONTENT:NAME')}
+      </ColHead>
+      <ColHead {...props} className="col-xs-2 col-sm-2" sortField="metadata.namespace">
+        {t('CONTENT:NAMESPACE')}
+      </ColHead>
+      <ColHead {...props} className="col-sm-2 hidden-xs" sortField="metadata.creationTimestamp">
+        {t('CONTENT:CREATED')}
+      </ColHead>
+    </ListHeader>
+  );
 };
 
 const WorkflowTemplateRow = () =>
-    // eslint-disable-next-line no-shadow
-    function WorkflowTemplateRow({ obj }) {
-        return (
-            <div className="row co-resource-list__item">
-                <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
-                    <ResourceCog actions={menuActions} kind="WorkflowTemplate" resource={obj} />
-                    <ResourceLink kind="WorkflowTemplate" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
-                </div>
-                <div className="col-xs-2 col-sm-2 co-break-word">{obj.metadata.namespace}</div>
-                <div className="col-xs-2 col-sm-2 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
-            </div>
-        );
-    };
+  // eslint-disable-next-line no-shadow
+  function WorkflowTemplateRow({ obj }) {
+    return (
+      <div className="row co-resource-list__item">
+        <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
+          <ResourceCog actions={menuActions} kind="WorkflowTemplate" resource={obj} />
+          <ResourceLink kind="WorkflowTemplate" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
+        </div>
+        <div className="col-xs-2 col-sm-2 co-break-word">{obj.metadata.namespace}</div>
+        <div className="col-xs-2 col-sm-2 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
+      </div>
+    );
+  };
 
 const Details = ({ obj }) => {
-    const { t } = useTranslation();
-    return (
-        <React.Fragment>
-            <ScrollToTopOnMount />
-            <div className="co-m-pane__body">
-                <SectionHeading text={t('ADDITIONAL:OVERVIEWTITLE', { something: ResourcePlural('workflowtemplate', t) })} />
-                <div className="row">
-                    <div className="col-sm-6">
-                        <ResourceSummary resource={obj} />
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    );
+  const { t } = useTranslation();
+  return (
+    <React.Fragment>
+      <ScrollToTopOnMount />
+      <div className="co-m-pane__body">
+        <SectionHeading text={t('ADDITIONAL:OVERVIEWTITLE', { something: ResourcePlural('workflowtemplate', t) })} />
+        <div className="row">
+          <div className="col-sm-6">
+            <ResourceSummary resource={obj} />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export const WorkflowTemplateList = props => {
-    const { kinds } = props;
-    const Row = WorkflowTemplateRow(kinds[0]);
-    Row.displayName = 'WorkflowTemplateRow';
-    return <List {...props} Header={WorkflowTemplateHeader} Row={Row} />;
+  const { kinds } = props;
+  const Row = WorkflowTemplateRow(kinds[0]);
+  Row.displayName = 'WorkflowTemplateRow';
+  return <List {...props} Header={WorkflowTemplateHeader} Row={Row} />;
 };
 WorkflowTemplateList.displayName = WorkflowTemplateList;
 
 export const WorkflowTemplatePage = props => {
-    const { t } = useTranslation();
-    return <ListPage {...props} ListComponent={WorkflowTemplateList} createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(props.kind, t) })} canCreate={true} kind="WorkflowTemplate" />;
+  const { t } = useTranslation();
+  return <ListPage {...props} ListComponent={WorkflowTemplateList} createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(props.kind, t) })} canCreate={true} kind="WorkflowTemplate" />;
 };
 WorkflowTemplatePage.displayName = 'WorkflowTemplatePage';
 
 export const WorkflowTemplateDetailsPage = props => {
-    const { t } = useTranslation();
-    return (
-        <DetailsPage
-            {...props}
-            kind="WorkflowTemplate"
-            menuActions={menuActions}
-            pages={[navFactory.details(Details, t('CONTENT:OVERVIEW')), navFactory.editYaml()]}
-        />
-    );
+  const { t } = useTranslation();
+  return <DetailsPage {...props} kind="WorkflowTemplate" menuActions={menuActions} pages={[navFactory.details(Details, t('CONTENT:OVERVIEW')), navFactory.editYaml()]} />;
 };
 
 WorkflowTemplateDetailsPage.displayName = 'WorkflowTemplateDetailsPage';
