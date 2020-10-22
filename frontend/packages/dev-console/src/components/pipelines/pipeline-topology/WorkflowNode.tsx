ef4d0fdc17be32b1505a1573c166dbcf52777932
miri_jo <miri_jo@tmax.co.kr>
2020-10-22 16:16:57 +0900

    [feat] 워크플로우용 노드 생성 및 툴팁 보이지 않도록 수정, 태스크 서비스콜 하지 않도록 수정

---
+++ frontend/packages/dev-console/src/components/pipelines/pipeline-topology/WorkflowNode.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { observer, Node, NodeModel } from '../../../../../topology/src';
+import { pipelineRunFilterReducer } from '../../../utils/pipeline-filter-reducer';
+import { PipelineVisualizationTask } from '../detail-page-tabs/pipeline-details/PipelineVisualizationTask';
+import { DROP_SHADOW_SPACING } from './const';
+import { TaskNodeModelData } from './types';
+
+type TaskNodeProps = {
+  element: Node<NodeModel, TaskNodeModelData>;
+  disableTooltip?: boolean;
+};
+
+const WorkflowNode: React.FC<TaskNodeProps> = ({ element }) => {
+  const { height, width } = element.getBounds();
+  const { pipeline, pipelineRun, task, selected } = element.getData();
+
+  return (
+    <foreignObject width={width} height={height + DROP_SHADOW_SPACING}>
+      <PipelineVisualizationTask
+        pipelineRunName={pipelineRun?.metadata?.name}
+        task={task}
+        pipelineRunStatus={pipelineRun && pipelineRunFilterReducer(pipelineRun)}
+        namespace={pipeline?.metadata?.namespace}
+        disableTooltip={true}
+        selected={selected}
+      />
+    </foreignObject>
+  );
+};
+
+export default observer(WorkflowNode);
--- frontend/packages/dev-console/src/components/pipelines/pipeline-topology/const.ts
+++ frontend/packages/dev-console/src/components/pipelines/pipeline-topology/const.ts
@@ -17,7 +17,8 @@ export enum NodeType {
   SPACER_NODE = 'spacer',
   TASK_LIST_NODE = 'task-list',
   BUILDER_NODE = 'builder',
-  INVALID_TASK_LIST_NODE = 'invalid-task-list'
+  INVALID_TASK_LIST_NODE = 'invalid-task-list',
+  WORKFLOW_NODE = 'workflow'
 }
 export enum DrawDesign {
   INTEGRAL_SHAPE = 'integral-shape',
